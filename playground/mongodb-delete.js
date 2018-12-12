0a1f48d23afc73438e584105eed734aca1a3e1c9
Piyush Sharma <piyushsharma08@outlook.com>
2018-12-12 19:57:16 +0530

    delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,36 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, db)=>{
+    if(err){
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    
+    // Delete many
+    // db.collection('Todos').deleteMany({text:'eat dinner'}).then((result)=>{
+    //     console.log(result);
+    // });
+
+    //deleteOne
+    // db.collection('Todos').deleteOne({text:'eat dinner'}).then((result)=>{
+    //     console.log(result);
+    // });
+
+    //findOneAnddelete
+    // db.collection('Todos').findOneAndDelete({completed:false}).then((result)=>{
+    //     console.log(result);
+    // });
+
+    db.collection('Users').deleteMany({name:'Piyush Sharma'}).then((result)=>{
+        console.log(result.result);
+    });
+    db.collection('Users').findOneAndDelete({_id:new ObjectID("5c111214235fed0da9fa0f72")}).then((result)=>{
+        console.log(JSON.stringify(result,undefined,2));
+    });
+
+
+
+
+    db.close();
+});
